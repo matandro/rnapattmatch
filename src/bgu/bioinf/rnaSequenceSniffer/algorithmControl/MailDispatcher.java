136622716492842a8a72e8ba685d9ebc1c3a9857
Matan Drory <matan.drory@gmail.com>
2019-07-11 14:12:21 +0300

    Now working with a different mail using barash lab post address. Needs a provided class provided by tomcat with email credentials

--- src/bgu/bioinf/rnaSequenceSniffer/algorithmControl/MailDispatcher.java
+++ src/bgu/bioinf/rnaSequenceSniffer/algorithmControl/MailDispatcher.java
@@ -1,126 +1,135 @@
 package bgu.bioinf.rnaSequenceSniffer.algorithmControl;
 
+import bgu.bioinf.brlab.credentials.EmailCredentials;
 import bgu.bioinf.rnaSequenceSniffer.Model.JobInformation;
-import sun.net.smtp.SmtpClient;
 
-import java.io.IOException;
-import java.io.PrintStream;
+import javax.mail.*;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 import java.util.Map;
+import java.util.Properties;
 
 /**
  * Created by matan on 27/12/14.
  */
 public class MailDispatcher {
-    private static final String FROM_RNAPATTMATCH = "rnapattmatch@cs.bgu.ac.il";
-    private static final String SMTP_ADDRESS = "smtp.bgu.ac.il";
+    private static final String FROM_RNAPATTMATCH = EmailCredentials.USER_NAME;
+    private static final String SMTP_ADDRESS = EmailCredentials.SMTP_ADDRESS;
+
+    private static final String PASSWORD_RNAPATTMATCH = EmailCredentials.PASSWORD;
 
 
-    public static boolean submissionMail(JobInformation jobInformation) {
-        SmtpClient client = null;
-        if (jobInformation.getEmail() == null || "".equals(jobInformation.getEmail())) {
-            return false;
-        }
+    private static boolean sendMail(String bodyText, String subject, String recipient) {
+        Properties prop = new Properties();
+        prop.put("mail.smtp.host", SMTP_ADDRESS);
+        prop.put("mail.smtp.port", "465");
+        prop.put("mail.smtp.auth", "true");
+        prop.put("mail.smtp.socketFactory.port", "465");
+        prop.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
 
+        Session session = Session.getInstance(prop,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(FROM_RNAPATTMATCH, PASSWORD_RNAPATTMATCH);
+                    }
+                });
         boolean success = false;
         try {
-            client = new SmtpClient(SMTP_ADDRESS);
-            client.from(FROM_RNAPATTMATCH);
-            client.to(jobInformation.getEmail());
-            PrintStream message = client.startMessage();
-            message.println("From: RNA Pattern Matcher");
-            message.println("To: " + jobInformation.getEmail());
-            message.println("Subject: Results for query " + jobInformation.getQueryName());
-            message.print("Hello ");
-            message.println(jobInformation.getEmail() + ",");
-            message.println("Thank you for using our RNAPattMatch web server application.");
-            message.println("The search was started. Your job id is: " + jobInformation.getJobId());
-            message.println("Your results will be available in the following link:");
-            message.println();
-            message.println("http://www.cs.bgu.ac.il/rnapattmatch/GetResults.jsp?jid=" + jobInformation.getJobId());
-            message.println();
-            message.println("Results will be removed after a week.");
-            message.println("Have a good day!");
-            message.println();
-            message.flush();
-            message.close();
-            client.closeServer();
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(FROM_RNAPATTMATCH));
+            message.setRecipients(
+                    Message.RecipientType.TO,
+                    InternetAddress.parse(recipient)
+            );
+            message.setSubject(subject);
+            message.setText(bodyText);
+            Transport.send(message);
             success = true;
-        } catch (IOException e) {
+        } catch (AddressException e) {
             e.printStackTrace();
-        } finally {
-            if (client != null) {
-                try {
-                    client.closeServer();
-                } catch (IOException ignore) {
-                }
-            }
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         return success;
     }
 
+
+    public static boolean submissionMail(JobInformation jobInformation) {
+        if (jobInformation.getEmail() == null || "".equals(jobInformation.getEmail())) {
+            return false;
+        }
+
+        StringBuilder message = new StringBuilder();
+        message.append("Hello ");
+        message.append(jobInformation.getEmail());
+        message.append(",\n");
+        message.append("Thank you for using our RNAPattMatch web server application.\n");
+        if (jobInformation.getQueryName() != null && !"".equals(jobInformation.getQueryName())) {
+            message.append("The search was started for query: ");
+            message.append(jobInformation.getQueryName());
+            message.append(". Your job id is: ");
+        } else {
+            message.append("The search was started. Your job id is: ");
+        }
+        message.append(jobInformation.getJobId());
+        message.append("\nYour results will be available in the following link:\n");
+        message.append("http://www.cs.bgu.ac.il/rnapattmatch/GetResults.jsp?jid=");
+        message.append(jobInformation.getJobId());
+        message.append("\nResults will be removed after a week.\n\nThis is an automatic e-mail, " +
+                "replays are ignored.\nHave a good day!\n");
+        return sendMail(message.toString(), "RNAPattMatch confirmed submission, Query name: "
+                + jobInformation.getQueryName(), jobInformation.getEmail());
+    }
+
     public static boolean calculationCompleteMail(JobRunner jobRunner) {
-        SmtpClient client = null;
         JobInformation jobInformation = jobRunner.getJobInformation();
         if (jobInformation.getEmail() == null || "".equals(jobInformation.getEmail())) {
             return false;
         }
 
-        boolean success = false;
-        try {
-            client = new SmtpClient(SMTP_ADDRESS);
-            client.from(FROM_RNAPATTMATCH);
-            client.to(jobInformation.getEmail());
-            PrintStream message = client.startMessage();
-            message.println("From: RNA Pattern Matcher");
-            message.println("To: " + jobInformation.getEmail());
-            message.println("Subject: Query \"" + jobInformation.getQueryName() + "\" is ready");
-            message.print("Hello ");
-            message.println(jobInformation.getEmail() + ",");
-            message.print("Your search is done, ");
-
-            switch (jobRunner.getJobState()) {
-                case ERROR:
-                    message.println("an error occurred.");
-                    break;
-                case DONE_NO_RESULTS:
-                    message.println("no results were found.");
-                    break;
-                case DONE_RESULTS:
-                    Map<Integer, Integer> targetResults = jobRunner.getTargetResults();
-                    message.println(jobRunner.getTotalResults() + " results were found for " +
-                            targetResults.size() + " targets.");
-                    message.println("Results per target (order as in file):");
-                    for (Map.Entry<Integer, Integer> entry : targetResults.entrySet()) {
-                        message.println("Target " + entry.getKey() + ": " + entry.getValue() + " results.");
-                    }
-                    break;
-                default:
-                    message.println("Unknown search replay...");
-                    break;
-            }
-            message.println();
-            message.println("Reminder! Your job id is: " + jobInformation.getJobId());
-            message.println("Additional information on the results is available in the following link:");
-            message.println();
-            message.println("http://www.cs.bgu.ac.il/rnapattmatch/GetResults.jsp?jid=" + jobInformation.getJobId());
-            message.println();
-            message.println("Results will be removed after a week.");
-            message.println("Have a good day!");
-            message.println();
-            message.flush();
-            message.close();
-            client.closeServer();
-            success = true;
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (client != null) {
-                try {
-                    client.closeServer();
-                } catch (IOException ignore) {
+        StringBuilder message = new StringBuilder();
+        message.append("Hello ");
+        message.append(jobInformation.getEmail());
+        message.append(",\nYour search is done, ");
+        switch (jobRunner.getJobState()) {
+            case ERROR:
+                message.append("an error occurred.\n");
+                break;
+            case DONE_NO_RESULTS:
+                message.append("no results were found.\n");
+                break;
+            case DONE_RESULTS:
+                Map<Integer, Integer> targetResults = jobRunner.getTargetResults();
+                message.append(jobRunner.getTotalResults());
+                message.append(" results were found for ");
+                message.append(targetResults.size());
+                message.append(" targets.\nResults per target (order as in file):\n");
+                for (Map.Entry<Integer, Integer> entry : targetResults.entrySet()) {
+                    message.append("Target ");
+                    message.append(entry.getKey());
+                    message.append(": ");
+                    message.append(entry.getValue());
+                    message.append(" results.\n");
                 }
-            }
+                break;
+            default:
+                message.append("Unknown search replay... try again, if the error persist contact the email " +
+                        "noted in the site\n");
+                break;
         }
-        return success;
+        message.append("\nReminder! Your job id is: ");
+        message.append(jobInformation.getJobId());
+        message.append("\nYou can view the results in the following link:\n");
+        message.append("http://www.cs.bgu.ac.il/rnapattmatch/GetResults.jsp?jid=");
+        message.append(jobInformation.getJobId());
+        message.append("\nResults will be removed after a week.\n\nThis is an automatic e-mail, " +
+                "replays are ignored.\nHave a good day!\n");
+
+        return sendMail(message.toString(), "RNAPattMatch results are ready!, Query name: "
+                + jobInformation.getQueryName(), jobInformation.getEmail());
     }
 }
